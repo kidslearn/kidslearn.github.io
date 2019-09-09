98428dd5a5f8ccf15a50295ec0092facb99ffabf
jinniegithub <jintian1984@gmail.com>
2019-09-08 22:16:58 -0700

    .

---
+++ precache-manifest.e6cc2fbab99c726d441770310c33d6cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31e53a65f76b21aa0ee46496e7926f5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a446912d8f7ca0d40c1",
+    "url": "/static/css/2.56d65a16.chunk.css"
+  },
+  {
+    "revision": "c80c422394fa24dd96eb",
+    "url": "/static/css/main.2f6f65b0.chunk.css"
+  },
+  {
+    "revision": "6a446912d8f7ca0d40c1",
+    "url": "/static/js/2.ffe02a4a.chunk.js"
+  },
+  {
+    "revision": "c80c422394fa24dd96eb",
+    "url": "/static/js/main.40ae500b.chunk.js"
+  },
+  {
+    "revision": "223d1dfe40295a5bb600",
+    "url": "/static/js/runtime~main.5278af6a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
